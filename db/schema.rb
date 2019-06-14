0d85b3d1064edee0ee67414e6b86d6b1b28c8a8d
AsliUral <aasliuural@gmail.com>
2019-06-14 21:40:14 +0300

    Updated new.html and image_uploader.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190612131358) do
+ActiveRecord::Schema.define(version: 20190614175813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20190612131358) do
     t.string "recipe_tags"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
