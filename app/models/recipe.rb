5b02d0cfef4125c6c763dd7873620efc97b22407
AsliUral <aasliuural@gmail.com>
2019-06-16 03:23:33 +0300

    Search Tag Model for exact matching

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,19 @@
 class Recipe < ApplicationRecord
     mount_uploader :image, ImageUploader
+  
+
+    def self.searchTag(searchTag)
+        
+        if searchTag
+           recipe2 = Recipe.where(:recipe_tags => searchTag)
+
+            if recipe2 
+                self.where(id: recipe2) 
+                
+            end
+        
+            else
+                Recipe.all
+        end
+    end
 end
