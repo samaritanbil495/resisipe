0b663a231146a300b943c39dab1da867733386aa
Ediz Uslu <36105513+edizuslu@users.noreply.github.com>
2019-07-13 16:03:22 +0300

    update routes.rb for like

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,9 @@ end
 
   get 'andrecipes/edit'
 
-	resources :recipes
+	resources :recipes do
+    		resources :likes
+  	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
