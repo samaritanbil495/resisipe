2f3b1ea802a43b9dabc96408a23204dd7d30299a
AsliUral <aasliuural@gmail.com>
2019-07-13 16:08:46 +0300

    Created like model and add to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190711181511) do
+ActiveRecord::Schema.define(version: 20190713114009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "recipe_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_likes_on_recipe_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "recipe_id"
@@ -34,4 +43,6 @@ ActiveRecord::Schema.define(version: 20190711181511) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "likes", "recipes"
+  add_foreign_key "likes", "users"
 end
