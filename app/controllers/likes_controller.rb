403243e5669479652c4a0df8fc52836328e5ef44
AsliUral <aasliuural@gmail.com>
2019-07-24 21:58:16 +0300

    Last Check

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,25 @@
- class LikesController < ApplicationController
-	    before_action :current_recipe
-	    skip_before_action :verify_authenticity_token
+class LikesController < ApplicationController
+	before_action :current_recipe
+	skip_before_action :verify_authenticity_token
 
-	   def create
-	      @recipe.likes.create(user_id: 1)
+   def create
+	 if already_liked?
+	  flash[:notice] = "You can't like more than once"
+	else
+	  @recipe.likes.create(user_id: current_user.id)
+	end
+	redirect_to recipe_path(@recipe)
+   end
 
-	    redirect_to recipe_path(@recipe)
-	   end
-
-	   private
-	   def recipe_params
-	    params.require(:recipe).permit(:recipe_name, :recipe_detail, :recipe_ingredients, :recipe_tags, :search, :recipe_id, :image, :image_cache)
-	  end
-	  def already_liked?
-	    Like.where(user_id: current_user.id, recipe_id:
-	    params[:recipe_id]).exists?
-	  end
-	   def current_recipe
-	    @recipe = Recipe.find(params[:recipe_id])
-	  end
-	 end
+   private
+   def recipe_params
+	params.require(:recipe).permit(:recipe_name, :recipe_detail, :recipe_ingredients, :recipe_tags, :search, :recipe_id, :image, :image_cache)
+  end
+  def already_liked?
+	Like.where(user_id: current_user.id, recipe_id:
+	params[:recipe_id]).exists?
+  end
+   def current_recipe
+	@recipe = Recipe.find(params[:recipe_id])
+  end
+ end 
